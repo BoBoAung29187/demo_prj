194ef33fe89b2eb81c73e8560b91498fa383e2ba
BoBo <bobo@blue-stone.net>
2022-03-07 16:17:13 +0630

    mod

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,11 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },  {
+    path: 'login-user',
+    loadChildren: () => import('./pages/login-user/login-user.module').then( m => m.LoginUserPageModule)
   }
+
 ];
 
 @NgModule({
